f550371cc90820befa5955ce13f40a5033fc7bdd
Craabby <>
2021-06-02 18:25:15 +0000

    fix errr

--- packets/makeName.js
+++ packets/makeName.js
@@ -1,3 +1,3 @@
 module.exports = (ws, name) => {
-  return (msg.name || "Unnamed").split('').splice(0, 15).join('') + ws.tag;
+  return (name || "Unnamed").split('').splice(0, 15).join('') + ws.tag;
 }
--- packets/onJoin.js
+++ packets/onJoin.js
@@ -1,4 +1,4 @@
 module.exports = (ws, msg, makeName) => {
   ws.server = msg.server;
-  ws.name = ws.name ? ws.name : makeName(ws, msg);
+  ws.name = ws.name ? ws.name : makeName(ws, msg.name);
 }
